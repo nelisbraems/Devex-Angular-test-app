569c512873b0ee8f2b175961322d647b617c30b7
Nelis Braems <nelis.braems@globis-software.com>
2021-03-12 14:51:10 +0100

    Textbox basic dx component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TextboxComponent } from './textbox/textbox.component';
+import { DxTextBoxModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextboxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxTextBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
